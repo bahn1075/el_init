dc5adcfac4f4ed8b534f99d798fb5fb1b98c5da1
bahn1075 <66509975+bahn1075@users.noreply.github.com>
2025-05-09 10:13:00 +0900

    Update 13.zsh_setup.sh

--- 13.zsh_setup.sh
+++ 13.zsh_setup.sh
@@ -1,11 +1,10 @@
 ## install zsh
 sudo dnf install zsh -y
 ## oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ## 설치 직후 zsh 적용
-zsh
-## 현재 사용자의 기본 shell을 zsh로 변경
-sudo usermod -s /usr/bin/zsh $USER
+#zsh
+
 
 ## p110k theme apply
 ## git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
@@ -16,3 +15,6 @@ sudo usermod -s /usr/bin/zsh $USER
 mkdir -p "$HOME/.zsh"
 git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.zsh/spaceship"
 source "$HOME/.zsh/spaceship/spaceship.zsh"
+
+## 현재 사용자의 기본 shell을 zsh로 변경
+sudo usermod -s /usr/bin/zsh $USER
