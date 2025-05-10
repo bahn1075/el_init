049adbf6b56726fd317e266c8a2a41c5d1685670
bahn1075 <66509975+bahn1075@users.noreply.github.com>
2025-05-11 08:52:12 +0900

    Update 13.zsh_setup.sh

--- 13.zsh_setup.sh
+++ 13.zsh_setup.sh
@@ -1,21 +1,11 @@
 ## install zsh
 sudo dnf install zsh -y
 ## oh my zsh
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-## 설치 직후 zsh 적용
-#zsh
-
-
-## p110k theme apply
-## git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-## .zshrc에 p10k 적용
-## sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ## spaceship prompt로 변경
-mkdir -p "$HOME/.zsh"
-git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.zsh/spaceship"
-echo 'source "$HOME/.zsh/spaceship/spaceship.zsh"' >> ~/.zshrc
-source "$HOME/.zsh/spaceship/spaceship.zsh"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 ## 현재 사용자의 기본 shell을 zsh로 변경
 sudo usermod -s /usr/bin/zsh $USER
