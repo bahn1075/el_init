b1595b9287f1224ca7fcae526f286a9d0c279a8b
bahn1075 <66509975+bahn1075@users.noreply.github.com>
2025-05-11 11:45:12 +0900

    Update 9.install_essentail.sh

--- 9.install_essentail.sh
+++ 9.install_essentail.sh
@@ -1,4 +1,5 @@
-sudo dnf update -y
 sudo dnf install epel-release -y
-sudo dnf install btop bat zip vim curl wget -y
+sudo dnf install btop bat zip vim curl zsh wget fontconfig -y
 sudo dnf group install "Development Tools"
+sudo dnf update -y
+sudo usermod -s /usr/bin/zsh $USER
