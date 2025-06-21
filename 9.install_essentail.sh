f50069aba242668990ba8e21ca846e4f67710049
cozy <bahn1075@gmail.com>
2025-06-21 19:28:07 +0900

    lsof등 추가

--- 9.install_essentail.sh
+++ 9.install_essentail.sh
@@ -1,5 +1,5 @@
 sudo dnf install epel-release -y
-sudo dnf install btop bat zip vim curl zsh wget fontconfig -y
+sudo dnf install btop tree file bat zip vim curl zsh wget fontconfig lsof net-tools bind-utils -y
 sudo dnf group install "Development Tools"
 sudo dnf update -y
 sudo usermod -s /usr/bin/zsh $USER
