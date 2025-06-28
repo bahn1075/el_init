2aef4b2b0e5e008010cce0a7f473d9c274d27b7b
Cozy <bahn1075@gmail.com>
2025-06-28 13:18:13 +0900

    바이너리를 /tmp로 다운로드 하도록 변경

--- 72.kubectl_helm_latest.sh
+++ 72.kubectl_helm_latest.sh
@@ -6,13 +6,13 @@ echo "kubectl 및 helm 설치를 시작합니다..."
 
 # kubectl 설치
 echo "kubectl 설치를 진행합니다..."
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -Lo /tmp/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 if [ $? -ne 0 ]; then
   echo "kubectl 바이너리 다운로드에 실패했습니다."
   exit 1
 fi
 
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 /tmp/kubectl /usr/local/bin/kubectl
 if [ $? -ne 0 ]; then
   echo "kubectl 설치에 실패했습니다."
   exit 1
