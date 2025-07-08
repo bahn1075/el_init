e92208b9f530cc247e9a0c51f0e16393064af464
bahn1075 <66509975+bahn1075@users.noreply.github.com>
2025-07-09 08:58:55 +0900

    Update 10.install_essentail.sh
    wsl 특화 부분을 주석 처리하고
    TZ 서울 설정을 추가함

--- 10.install_essentail.sh
+++ 10.install_essentail.sh
@@ -1,13 +1,14 @@
-sudo tee /etc/wsl.conf > /dev/null <<'EOF'
-[user]
-default=cozy
-[boot]
-systemd=true
-EOF
+#sudo tee /etc/wsl.conf > /dev/null <<'EOF'
+#[user]
+#default=cozy
+#[boot]
+#systemd=true
+#EOF
+sudo timedatectl set-timezone Asia/Seoul
 sudo dnf install epel-release -y
-sudo dnf install btop tree file bat zip vim curl zsh wget fontconfig lsof net-tools bind-utils socat -y
+sudo dnf install btop tree file bat zip vim curl zsh wget fontconfig lsof net-tools bind-utils socat fastfetch -y
 sudo dnf group install "Development Tools"
 sudo dnf update -y
-sudo usermod -s /usr/bin/zsh $USER
-git config --global user.email "bahn1075@gmail.com"
-git config --global user.name "Cozy"
+#sudo usermod -s /usr/bin/zsh $USER
+#git config --global user.email "bahn1075@gmail.com"
+#git config --global user.name "Cozy"
