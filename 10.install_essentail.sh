d518212b5e64917a7828b11e40e86a3d22a2cb2c
bahn1075 <66509975+bahn1075@users.noreply.github.com>
2025-07-09 15:19:10 +0900

    Update 10.install_essentail.sh

--- 10.install_essentail.sh
+++ 10.install_essentail.sh
@@ -9,6 +9,8 @@ sudo dnf install epel-release -y
 sudo dnf install btop tree file bat zip vim curl zsh wget fontconfig lsof net-tools bind-utils socat fastfetch -y
 sudo dnf group install "Development Tools"
 sudo dnf update -y
+mkdir -p ~/.config/fastfetch
+cp ./fastfetch_config.jsonc ~/.config/fastfetch/config.jsonc
 #sudo usermod -s /usr/bin/zsh $USER
 #git config --global user.email "bahn1075@gmail.com"
 #git config --global user.name "Cozy"
